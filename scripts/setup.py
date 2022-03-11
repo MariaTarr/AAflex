d8b6f904c694c162489e980de75c1ec9cba279b5
MariaTarr <maria.tarradas01@estudiant.upf.edu>
2022-03-11 18:51:17 +0100

    Create setup.py

---
+++ scripts/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+#from setuptools import setup, find_packages
+
+setup(name='AminoAcids flexi',
+    version='1.0',
+    description='we don0t know wtf are doing',
+    author='Alejandro Madrid, Xavier Soler, Maria Tarradas',
+    author_email='maria.tarradas01@estudiant.upf.edu',
+    url='https://github.com/MariaTarr/AAflex.git',
+    keywords='',
+    packages=['', '']  #find_packages()
+    )
